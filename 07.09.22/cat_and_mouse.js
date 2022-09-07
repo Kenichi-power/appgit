dd9f61acbed65104cb1a192feba72a92cdc5bfe9
Kenes Baimukanov <kenes.baimukanov@gmail.com>
2022-09-07 13:50:33 +0500

    mod

--- 07.09.22/cat_and_mouse.js
+++ 07.09.22/cat_and_mouse.js
@@ -1,3 +1,6 @@
 function catMouse(x, j){
-
-}
+    if (!x.includes('D')||!x.includes('C')||!x.includes('m')) return "boring without all three"
+    if (Math.abs(x.indexOf('C')-x.indexOf('m'))>j) return "Escaped!"
+    if (x.replace(/\./g,'')==='CDm'||x.replace(/\./g,'')==='mDC') return 'Protected!'
+    return "Caught!"
+  }
